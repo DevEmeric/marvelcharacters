f3014cd03f7f89161612af87f99f3d954e8d0428
emeric <emeric555@gmail.com>
2019-02-11 22:25:34 +0100

    main project done

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,19 +1,48 @@
-import React from 'react';
-import { Card, CardImg, CardBody,
-  CardTitle, Button } from 'reactstrap';
+import React, { Component } from 'react';
+import CardsInfo from '../containers/CardsInfo';
+import {
+  Card, CardImg, CardBody,
+  CardTitle, Button
+} from 'reactstrap';
+import PropTypes from 'prop-types';
 
-const Cards = (characterInfo) => {
-  return (
-    <div>
-      <Card className="d-flex justify-content-around">
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-        <CardBody className="text-center">
-          <CardTitle>{characterInfo.name}</CardTitle>
-          <Button size="sm">Add to your favorites</Button>
-        </CardBody>
-      </Card>
-    </div>
-  );
-};
+class Cards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpenModal: false
+    }
+  }
+
+  handleClick = () => {
+    this.setState({ isOpenModal: !this.state.isOpenModal })
+  }
+
+  render() {
+    const { characters } = this.props;
+    const { isOpenModal } = this.state;
+    return (
+      <div>
+        <Card className="cardClass d-flex justify-content-around">
+          <CardImg className="cardImage" src={characters.thumbnail.path + '.' + characters.thumbnail.extension} alt="Card image cap" />
+          <CardBody className="text-center">
+            <CardTitle><Button onClick={() => this.handleClick()}>{characters.name}</Button></CardTitle>
+          </CardBody>
+        </Card>
+        {
+          isOpenModal ?
+          <CardsInfo modalState={isOpenModal} details={characters} />
+          :
+          null
+        }
+      </div >
+    );
+  };
+}
+
+
+Cards.propTypes = {
+  characters: PropTypes.object,
+}
 
 export default Cards;
--- client/src/containers/List.jsx
+++ client/src/containers/List.jsx
@@ -4,24 +4,31 @@ import { Row, Col } from 'reactstrap';
 import Cards from '../components/Cards';
 
 class List extends Component {
-  state = {
-    list: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      characters: []
+    }
   }
 
   componentDidMount() {
     axios.get('api')
-     .then(res => console.log('didmount res:', res.data.results))
-     .catch(err => console.log(err))
+      .then((res) => {
+        const fetchedList = [res.data.results];
+        this.setState({ characters: fetchedList[0] })
+      })
+      .catch(err => console.log(err))
   }
 
- 
 
   render() {
+    const { characters } = this.state;
     return (
       <Row className="fullList">
-        <Col xs="3" md="3">
-          <Cards />
-        </Col>
+        {characters.map(item =>
+          <Col xs="3" md="3" key={item.id}>
+            <Cards characters={item} />
+          </Col>)}
       </Row>
     );
   }
